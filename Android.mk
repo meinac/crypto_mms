17549d93cc1c9e0c6a73e2e0e470b760e2251dc3
Mehmet Emin İNAÇ <mehmetemininac@gmail.com>
2015-04-11 00:15:33 +0300

    add commons-codec jar as a dependency

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_JAVA_LIBRARIES += telephony-common mms-common
 LOCAL_STATIC_JAVA_LIBRARIES += android-common jsr305
 LOCAL_STATIC_JAVA_LIBRARIES += android-common-chips
 LOCAL_STATIC_JAVA_LIBRARIES += core-2.3.0
+LOCAL_STATIC_JAVA_LIBRARIES += commons-codec-1.10
 
 LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
 LOCAL_AAPT_FLAGS := --auto-add-overlay
@@ -37,6 +38,7 @@ include $(BUILD_PACKAGE)
 include $(CLEAR_VARS)
 
 LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := core-2.3.0:libs/core-2.3.0.jar
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := commons-codec-1.10:libs/commons-codec-1.10.jar
 include $(BUILD_MULTI_PREBUILT)
 
 # This finds and builds the test apk as well, so a single make does both.
