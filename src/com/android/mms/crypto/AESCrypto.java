d4e8c4803eb915290aa065496601a1322526d900
Mehmet Emin İNAÇ <mehmetemininac@gmail.com>
2015-04-25 22:39:11 +0300

    AESCrypto class added

---
+++ src/com/android/mms/crypto/AESCrypto.java
@@ -0,0 +1,28 @@
+package com.android.mms.crypto;
+
+public class AESCrypto {
+
+  //returns randomly generated aes key as String
+  public String generateAESKey() {
+
+  }
+
+  /*
+  * encrypts and encodes given string 
+  * using given pair's session_key 
+  * returns encoded message
+  */
+  public String encrypt(Pair pair, String message) {
+
+  }
+
+  /*
+  * decodes and decrypts given encrypted string 
+  * using given pair's session_key
+  * returns original message
+  */
+  public String decrypt(Pair pair, String message) {
+
+  }
+
+}
