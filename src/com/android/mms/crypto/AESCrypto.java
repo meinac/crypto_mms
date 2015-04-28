d7fc03cc423471288cd1e51bc811697dd3bffbbf
hakan <tkhakan@gmail.com>
2015-04-29 02:43:10 +0300

    AESCrypto class created

---
+++ src/com/android/mms/crypto/AESCrypto.java
@@ -0,0 +1,103 @@
+package com.android.mms.crypto;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.Base64;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.android.mms.crypto_models.Pair;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.SecureRandom;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESCrypto {
+
+
+
+    SecretKeySpec sks = null;
+    byte[] sksbyte;
+    String sessionKey;
+    String encoded;
+    byte[] encryptedBytes = null;
+    byte[] decryptedBytes = null;
+    static final String TAG = "SymmetricAlgorithmAES";
+
+    //returns randomly generated aes key as String
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public String generateAESKey() {
+
+
+        try {
+            SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+            sr.setSeed("any data used as random seed".getBytes());
+            KeyGenerator kg = KeyGenerator.getInstance("AES");
+            kg.init(256, sr);
+            sks = new SecretKeySpec((kg.generateKey()).getEncoded(), "AES");
+            sksbyte = sks.getEncoded();
+            sessionKey = Base64.encodeToString(sksbyte, Base64.DEFAULT);
+
+        } catch (Exception e) {
+            Log.e(TAG, "AES secret key spec error");
+        }
+            return  sessionKey;
+
+
+    }
+
+
+
+    /*
+    * encrypts and encodes given string
+    * using given pair's session_key
+    * returns encoded message
+    */
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public String encrypt(Pair pair, String message) {
+        try {
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.ENCRYPT_MODE,sks);
+            encryptedBytes = c.doFinal(message.getBytes());
+        } catch (Exception e) {
+            Log.e(TAG, "AES encryption error");
+        }
+        encoded=Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
+
+
+
+        return encoded;
+    }
+
+    /*
+    * decodes and decrypts given encrypted string
+    * using given pair's session_key
+    * returns original message
+    */
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public byte[] decrypt(Pair pair, String encoded) {
+
+
+        try {
+            byte[] decoded=Base64.decode(encoded,Base64.DEFAULT);
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.DECRYPT_MODE, sks);
+            decryptedBytes = c.doFinal(decoded);
+        } catch (Exception e) {
+            Log.e(TAG, "AES decryption error");
+        }
+
+        return decryptedBytes;
+    }
+
+
+
+
+
+}
--- src/com/android/mms/crypto/AESDeneme.java
+++
@@ -1,8 +0,0 @@
-package com.android.mms.crypto;
-
-/**
- * Created by hakan on 27.04.2015.
- */
-public class AESDeneme {
-
-}
