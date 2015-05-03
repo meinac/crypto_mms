6c22f97ba760f3cb7308732a22c1f48232606f24
hakan <tkhakan@gmail.com>
2015-05-03 23:20:52 +0300

    AES class changed

--- src/com/android/mms/crypto/AESCrypto.java
+++ src/com/android/mms/crypto/AESCrypto.java
@@ -22,18 +22,17 @@ public class AESCrypto {
 
 
 
-    SecretKeySpec sks = null;
-    byte[] sksbyte;
-    String sessionKey;
-    String encoded;
-    byte[] encryptedBytes = null;
+
+
     byte[] decryptedBytes = null;
     static final String TAG = "SymmetricAlgorithmAES";
 
     //returns randomly generated aes key as String
     @TargetApi(Build.VERSION_CODES.FROYO)
     public String generateAESKey() {
-
+        SecretKeySpec sks = null;
+        byte[] sksbyte;
+        String sessionKey;
 
         try {
             SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
@@ -43,14 +42,13 @@ public class AESCrypto {
             sks = new SecretKeySpec((kg.generateKey()).getEncoded(), "AES");
             sksbyte = sks.getEncoded();
             sessionKey = Base64.encodeToString(sksbyte, Base64.DEFAULT);
-
+            return sessionKey;
         } catch (Exception e) {
             Log.e(TAG, "AES secret key spec error");
         }
-            return  sessionKey;
-
-
+        return  null;
     }
+
 
 
 
@@ -61,17 +59,17 @@ public class AESCrypto {
     */
     @TargetApi(Build.VERSION_CODES.FROYO)
     public String encrypt(Pair pair, String message) {
+        String encoded;
+        byte[] encryptedBytes = null;
         try {
             Cipher c = Cipher.getInstance("AES");
-            c.init(Cipher.ENCRYPT_MODE,sks);
+            SecretKeySpec sks = new SecretKeySpec(pair.sessionKey.getBytes(), "AES");
+            c.init(Cipher.ENCRYPT_MODE, sks);
             encryptedBytes = c.doFinal(message.getBytes());
         } catch (Exception e) {
             Log.e(TAG, "AES encryption error");
         }
-        encoded=Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
-
-
-
+        encoded = Base64.encodeToString(encryptedBytes, Base64.DEFAULT);
         return encoded;
     }
 
@@ -81,19 +79,17 @@ public class AESCrypto {
     * returns original message
     */
     @TargetApi(Build.VERSION_CODES.FROYO)
-    public byte[] decrypt(Pair pair, String encoded) {
-
-
+    public String decrypt(Pair pair, String encoded) {
         try {
-            byte[] decoded=Base64.decode(encoded,Base64.DEFAULT);
+            byte[] decoded = Base64.decode(encoded,Base64.DEFAULT);
             Cipher c = Cipher.getInstance("AES");
+            SecretKeySpec sks = new SecretKeySpec(pair.sessionKey.getBytes(), "AES");
             c.init(Cipher.DECRYPT_MODE, sks);
             decryptedBytes = c.doFinal(decoded);
         } catch (Exception e) {
             Log.e(TAG, "AES decryption error");
         }
-
-        return decryptedBytes;
+        return decryptedBytes.toString();
     }
 
 
